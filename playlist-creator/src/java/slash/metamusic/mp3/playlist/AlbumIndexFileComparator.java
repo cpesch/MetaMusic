9a9558c6a7d127180ad5b7ba979f910a23b7f561
unknown <Christian@.(none)>
2010-08-21 19:16:14 +0200

    use part of set index if available

--- playlist-creator/src/java/slash/metamusic/mp3/playlist/AlbumIndexFileComparator.java
+++ playlist-creator/src/java/slash/metamusic/mp3/playlist/AlbumIndexFileComparator.java
@@ -25,15 +25,26 @@ class AlbumIndexFileComparator implements Comparator<MP3File> {
     public int compare(MP3File f1, MP3File f2) {
         String album1 = f1.getAlbum();
         String album2 = f2.getAlbum();
+
+        // to parse "Same album name" but different part of set index
         if (album1 != null && album1.equals(album2)) {
-            int index1 = f1.getIndex();
-            int index2 = f2.getIndex();
-            return index1 - index2;
+            int partOfSetIndex1 = f1.getPartOfSetIndex();
+            int partOfSetIndex2 = f2.getPartOfSetIndex();
+            if (partOfSetIndex1 != -1 && partOfSetIndex1 != partOfSetIndex2)
+                return partOfSetIndex1 - partOfSetIndex2;
+
+            int trackIndex1 = f1.getIndex();
+            int trackIndex2 = f2.getIndex();
+            return trackIndex1 - trackIndex2;
         }
-        int index1 = DiscIndexHelper.parseDiscIndex(album1);
-        int index2 = DiscIndexHelper.parseDiscIndex(album2);
-        if (index1 != index2)
-            return index1 - index2;
+
+        // to parse "Album name" (disc "2")
+        int discIndex1 = DiscIndexHelper.parseDiscIndex(album1);
+        int discIndex2 = DiscIndexHelper.parseDiscIndex(album2);
+        if (discIndex1 != -1 && discIndex1 != discIndex2)
+            return discIndex1 - discIndex2;
+
+        // different albums
         return f1.getFile().compareTo(f2.getFile());
     }
 }
